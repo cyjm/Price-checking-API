b33f86399cb731878c4183a3c3e78eacc9391fe2
cyjm <ivan.korobkin@protonmail.com>
2018-04-23 12:21:53 +0300

    user login view

--- src/models/users/views.py
+++ src/models/users/views.py
@@ -1,11 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, request, session, url_for, render_template
+from werkzeug.utils import redirect
 
+from src.models.users.user import User
 
 user_blueprint = Blueprint('users', __name__)
 
-@user_blueprint.route('/login')
+@user_blueprint.route('/login', methods=['GET','POST'])
 def login_user():
-    pass
+    if request.method == 'POST':
+        #check login is valid
+        email = request.form['email']
+        password = request.form['hashed']
+
+        if User.is_login_valid(email,password):
+            session['email'] = email
+            return redirect(url_for(".user_alerts"))
+
+    return render_template("user/login.html")
 
 @user_blueprint.route('/register')
 def register_user():
