7abaf2e78940736cd8d0c880bf875826f1b1f74a
cyjm <ivan.korobkin@protonmail.com>
2018-05-14 14:05:40 +0300

    _id's for objects to use instead of mongodb object id's

--- src/models/items/item.py
+++ src/models/items/item.py
@@ -3,16 +3,18 @@ from src.common.database import Database
 from bs4 import BeautifulSoup
 import re
 import src.models.items.constants as ItemConstants
+import uuid
 
 
 class Item(object):
-    def __init__(self, name, url, store):
+    def __init__(self, name, url, store, _id=None):
         self.name = name
         self.url = url
         self.store = store
         tag_name = store.tag_name
         query_path = store.query_path
         self.price = self.load_price(tag_name, query_path)
+        self._id = uuid.uuid4().hex if _id is None else _id
 
     def __repr__(self):
         return "<Item {} at the URL: {}>".format(self.name, self.url)
@@ -23,6 +25,7 @@ class Item(object):
         soup = BeautifulSoup(content, "html.parser")
         element = soup.find(tag_name, query_path)
         string_price = element.text.replace(" ", "")
+        string_price = string_price.replace("\xa0", "")
 
         pattern = re.compile("(\d+.\d+)")
         match = pattern.search(string_price)
@@ -35,6 +38,7 @@ class Item(object):
 
     def json(self):
         return{
+            "_id": self._id,
             "name": self.name,
             "url": self.url
         }
--- src/models/stores/store.py
+++ src/models/stores/store.py
@@ -1,9 +1,13 @@
+import uuid
 
 
 class Store(object):
-    def __init__(self, name, url_prefix):
+    def __init__(self, name, url_prefix, tag_name, query_path, _id=None):
         self.name = name
         self.url_prefix = url_prefix
+        self.tag_name = tag_name
+        self.query_path = query_path
+        self._id = uuid.uuid4().hex if _id is None else _id
 
     def __repr__(self):
         return "<Store {}>".format(self.name)
