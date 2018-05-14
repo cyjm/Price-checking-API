2e50b440c9cac26aa00784f7888e07379f1d8f77
cyjm <ivan.korobkin@protonmail.com>
2018-05-14 14:19:24 +0300

    store model: saving to db and search by id/name/url prefix

--- src/models/stores/store.py
+++ src/models/stores/store.py
@@ -1,5 +1,8 @@
 import uuid
 
+from src.common.database import Database
+import src.models.stores.constants as StoreConstants
+
 
 class Store(object):
     def __init__(self, name, url_prefix, tag_name, query_path, _id=None):
@@ -11,3 +14,27 @@ class Store(object):
 
     def __repr__(self):
         return "<Store {}>".format(self.name)
+
+    def json(self):
+        return{
+            "_id": self._id,
+            "name": self.name,
+            "url_prefix": self.url_prefix,
+            "tag_name": self.tag_name,
+            "query_path": self.query_path
+        }
+
+    @classmethod
+    def get_by_id(cls, id):
+        return cls(**Database.find_one(StoreConstants.COLLECTION, {"_id": id}))
+
+    def save_to_mongo(self):
+        Database.insert(StoreConstants.COLLECTION, self.json())
+
+    @classmethod
+    def get_by_name(cls, store_name):
+        return cls(**Database.find_one(StoreConstants.COLLECTION, {"name": store_name}))
+
+    @classmethod
+    def get_by_url_prefix(cls, url_prefix):
+        return cls(**Database.find_one(StoreConstants.COLLECTION, {"url_prefix": {"$regex": '^{}'.format(url_prefix)}}))
